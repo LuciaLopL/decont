c9819d9aa86c332db229f2a649121e720f8bdc8e
Lucía López <lopezlopezlucia42@gmail.com>
2024-01-21 14:10:30 +0100

    Merge de los archivos con el mismo ID

--- scripts/merge_fastqs.sh
+++ scripts/merge_fastqs.sh
@@ -3,3 +3,11 @@
 # stored in the output directory specified by the second argument ($2).
 #
 # The directory containing the samples is indicated by the first argument ($1).
+directorio_origen=$1
+directorio_output=$2
+sampleid=$3
+
+mkdir -p "$directorio_output"
+cat "$directorio_origen"/"$sampleid"*.*.1s*.fastq.gz "$directorio_origen"/"$sampleid"*.*.2s*.fastq.gz > "$directorio_output"/"$sampleid".fastq.gz
+
+
